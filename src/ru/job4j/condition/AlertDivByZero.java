241a1ce3be092bc5d8f77ceea0ec19372cfbcd05
Deshka <Sakhalinskaya6>
2020-06-22 21:31:11 +1100

    4.1. Операторы ветвлений [#307201]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -10,7 +10,6 @@ public class AlertDivByZero {
             System.out.println("Could not div by 0");
         }        else if (number < 0) {
             System.out.println("This is negative numbers");
-
         }
     }
 }
