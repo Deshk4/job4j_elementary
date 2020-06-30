36a69ee52283cfdfe9dc4c9ebec616a2609a6e2f
Deshk4 <Lee_@list.ru>
2020-06-30 15:12:43 +1100

    6.0.1. Объявление массива. [#307228]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,18 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surname = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Nikoay";
+        names[1] = "Petr";
+        names[2] = "Yura";
+        names[1] = "Evgeniy";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
