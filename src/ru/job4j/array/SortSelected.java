4eaca9944ccb7a320507e27226cc68c3ba4cd91f
Deshk4 <Lee_@list.ru>
2020-07-10 14:00:49 +1100

    6.5.3. Сортировка выборкой. [#307213]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -3,10 +3,9 @@ package ru.job4j.array;
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length - 1);
-            int index = FindLoop.indexOfSet(data, min, 0, data.length - 1);
-            SwitchArray.swap(data, min, index);
-
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOfSet(data, min, i, data.length - 1);
+            SwitchArray.swap(data, i, index);
         }
         return data;
     }
--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -14,4 +14,20 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenSort3Numbers() {
+        int[] input = new int[] {3, 4, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 3, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort7Numbers() {
+        int[] input = new int[] {3, 4, 1, 2, 5, 7, 8};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 7, 8};
+        assertThat(result, is(expect));
+    }
 }
